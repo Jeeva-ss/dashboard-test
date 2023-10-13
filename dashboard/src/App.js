7a55496dbaed1f5dced7e665b0e2a1a330eb7093
Jeeva <48169919+Jeeva-ss@users.noreply.github.com>
2023-10-13 22:22:15 +0530

    initial commit 🚀

---
+++ dashboard/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-indigo-blue text-2xl font-bold'>Test</p>
+    </div>
+  );
+}
+
+export default App;
